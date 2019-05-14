9a2b03f82566845da35002b8185490c468968281
Administrator <uge3@163.com>
2019-05-14 17:27:44 +0800

    vue个人博客练习

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="add-blog">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+export default{
+	name: 'add-blog',
+	data () {
+	    return {
+	     msg:"add-blog"
+	  }
+	}
+}
+	
+</script>
+
+<style>
+</style>
